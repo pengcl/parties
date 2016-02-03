c46f3e3a52f5a49afb32bd9159dd75d64e39ec29
pengcl <3559996@qq.com>
2016-02-03 11:37:57 +0800

    nothing

--- client/parties/parties-list/parties-list.component.js
+++ client/parties/parties-list/parties-list.component.js
@@ -5,23 +5,43 @@ angular.module('socially').directive('partiesList', function () {
         controllerAs: 'partiesList',
         controller: function ($scope, $reactive) {
             $reactive(this).attach($scope);
+
             this.newParty = {};
+            this.perPage = 3;
+            this.page = 1;
+            this.sort = {
+                name: 1
+            };
+
             this.helpers({
                 parties: function () {
-                    return Parties.find({});
+                    return Parties.find({}, {
+                        sort: this.getReactively('sort')
+                    });
                 }
             });
 
-            this.addParty = function () {
+            this.subscribe('parties', function () {
+                return [
+                    {
+                        limit: parseInt(this.perPage),
+                        skip: parseInt((this.getReactively('page') - 1) * this.perPage),
+                        sort: this.getReactively('sort')
+                    }
+                ]
+            });
+
+            this.addParty = () => {
                 this.newParty.owner = Meteor.user()._id;
                 Parties.insert(this.newParty);
                 this.newParty = {};
             };
-            this.removeParty = function (party) {
+
+            this.removeParty = (party) => {
                 Parties.remove({
                     _id: party._id
                 });
-            };
+            }
         }
     }
 });
--- server/parties.js
+++ server/parties.js
@@ -1,30 +1,18 @@
 Meteor.publish("parties", function () {
-    return Parties.find({
-        $or: [
-            {
-                $and: [
-                    {
-                        "public": true
-                    },
-                    {
-                        "public": {
-                            $exists: true
-                        }
-                    }
-                ]
-            },
-            {
-                $and: [
-                    {
-                        owner: this.userId
-                    },
-                    {
-                        owner: {
-                            $exists: true
-                        }
-                    }
-                ]
-            }
+  return Parties.find({
+    $or: [
+      {
+        $and: [
+          {"public": true},
+          {"public": {$exists: true}}
         ]
-    });
+      },
+      {
+        $and: [
+          {owner: this.userId},
+          {owner: {$exists: true}}
+        ]
+      }
+    ]
+  });
 });
